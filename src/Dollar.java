c2e354f985359b76fad55026aedd9b944cf24f0a
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-12-02 23:21:56 +0100

    Initial commit with the first (failing) test

---
+++ src/Dollar.java
@@ -0,0 +1,18 @@
+public class Dollar {
+    /**
+        Class for handling dollar currency.
+     */
+    public int amount;
+
+    public Dollar(int amount) {
+
+    }
+
+    /**
+     * Multiply the amount by the multiplier.
+     * @param multiplier Multiplier of the base amount
+     */
+    public void times(int multiplier) {
+
+    }
+}
