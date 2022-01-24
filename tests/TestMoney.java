588879f95037dc021e43300769d2c2fd61768bd0
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-01-24 23:48:57 +0100

    Use factory method to create Dollar instances

--- tests/TestMoney.java
+++ tests/TestMoney.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 public class TestMoney {
     @Test
     public void testEquality() {
-        assertEquals(Money.dollar(5), new Dollar(5));
-        assertNotEquals(Money.dollar(5), new Dollar(6));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(6));
         assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.franc(5), Money.franc(6));
         assertFalse(Money.franc(10).equals(Money.dollar(10)));
