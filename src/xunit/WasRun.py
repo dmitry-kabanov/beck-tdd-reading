3e53ba58751844175328c149f33084a6a5bee66a
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-02-07 23:22:26 +0100

    Implement bootstrapping test

---
+++ src/xunit/WasRun.py
@@ -0,0 +1,7 @@
+class WasRun:
+    def __init__(self, name):
+        self.name = name
+        self.wasRun = None
+
+    def testmethod(self):
+        self.wasRun = 1
---
+++ tests/test.py
@@ -0,0 +1,6 @@
+from xunit.WasRun import WasRun
+
+test = WasRun("testmethod")
+print(test.wasRun)
+test.testmethod()
+print(test.wasRun)
