53c8fd8d88aa5c8b6c50770788734d65cf284fa0
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-02-07 23:40:19 +0100

    Reimplement the bootstrap test as a test case method

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,12 @@
+from xunit.testcase import TestCase
 from xunit.wasrun import WasRun
 
-test = WasRun("testmethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun("testmethod")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+if __name__ == "__main__":
+    TestCaseTest("test_running").run()
