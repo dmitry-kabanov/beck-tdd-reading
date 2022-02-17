0d6ccc81a6674f636ee198b02aa05b11d9b08b21
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-02-17 23:01:26 +0100

    Add setUp method

--- src/xunit/wasrun.py
+++ src/xunit/wasrun.py
@@ -3,7 +3,10 @@ from xunit.testcase import TestCase
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
+
+    def setUp(self):
         self.wasRun = None
+        self.wasSetUp = 1
 
     def testmethod(self):
         self.wasRun = 1
--- tests/test.py
+++ tests/test.py
@@ -4,9 +4,14 @@ from xunit.wasrun import WasRun
 class TestCaseTest(TestCase):
     def test_running(self):
         test = WasRun("testmethod")
-        assert not test.wasRun
         test.run()
         assert test.wasRun
 
+    def test_setup(self):
+        test = WasRun("testmethod")
+        test.run()
+        assert test.wasSetUp
+
 if __name__ == "__main__":
     TestCaseTest("test_running").run()
+    TestCaseTest("test_setup").run()
